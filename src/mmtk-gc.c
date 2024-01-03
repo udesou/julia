749a66892f7e278a83d6f64a08ed52a4fd379541
Eduardo Souza <ledusou@gmail.com>
2024-01-03 02:47:08 +0000

    Revert "Setting up the default to be a fixed heap size of 30Gb"
    This reverts commit 280df8ffef8ef83842d1c616c709a641ae6e4c26.

--- src/mmtk-gc.c
+++ src/mmtk-gc.c
@@ -393,7 +393,7 @@ void jl_gc_init(void)
         double max_size = strtod(max_size_gb, &p);
         max_heap_size = (long) 1024 * 1024 * 1024 * max_size;
     } else {
-        max_heap_size = (long) 1024 * 1024 * 1024 * 30; // setting up the default as fixed heap size of 30Gb
+        max_heap_size = (long) 1024 * 1024 * 1024 * 25; // setting up the default as fixed heap size of 25Gb
     }
 
     // Assert that the number of stock GC threads is 0; MMTK uses the number of threads in jl_options.ngcthreads
