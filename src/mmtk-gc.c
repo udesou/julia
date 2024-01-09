577ae245884eacccfaa6fb62859023324a0f3ed4
Eduardo Souza <ledusou@gmail.com>
2024-01-09 23:25:59 +0000

    Setting heap size to 30Gb

--- src/mmtk-gc.c
+++ src/mmtk-gc.c
@@ -383,7 +383,7 @@ void jl_gc_init(void)
         min_heap_size = 0; // setting up the default as fixed heap size
     }
 
-    // default max heap currently set as 70% the free memory in the system
+    // default max heap currently set as 30 Gb
     if (max_size_def != NULL) {
         char *p;
         double max_size = strtod(max_size_def, &p);
@@ -393,7 +393,7 @@ void jl_gc_init(void)
         double max_size = strtod(max_size_gb, &p);
         max_heap_size = (long) 1024 * 1024 * 1024 * max_size;
     } else {
-        max_heap_size = uv_get_free_memory() * 70 / 100; // setting up the default as fixed heap size of 70% of the available memory
+        max_heap_size = 1024 * 1024 * 1024 * 30; // setting up the default as fixed heap size of 30 Gb
     }
 
     // Assert that the number of stock GC threads is 0; MMTK uses the number of threads in jl_options.ngcthreads
